37a2284cab551bd7cb6412ea1453c1c44b78f251
Liis <liis.kasper@gmail.com>
2020-11-11 14:32:35 +0200

    chat server

---
+++ server.js
@@ -0,0 +1,100 @@
+const server = require('http').createServer()
+const options = {}
+const io = require('socket.io')(server, options)
+
+server.listen(3000)
+
+
+const botName = 'Bot '
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('joinRoom', ({ username, room }) => {
+    const user = userJoin(socket.id, username, room);
+
+    socket.join(user.room);
+
+    // Welcome current user; kuvab ainult antud kasutajale 
+    socket.emit('message', formatMessage(botName, 'Welcome to Chat!'));
+
+    // Broadcast when a user connects k6igile v54lja arvatud kasutaja ise 
+    socket.broadcast
+      .to(user.room)
+      .emit(
+        'message',
+        formatMessage(botName, `${user.username} has joined the chat`)
+      );
+
+    // io.emit() // kuvab k6igile
+
+    // Send users and room info
+    io.to(user.room).emit('roomUsers', {
+      room: user.room,
+      users: getRoomUsers(user.room)
+    });
+  });
+
+  // Listen for chatMessage
+  socket.on('chatMessage', msg => {
+    const user = getCurrentUser(socket.id);
+    console.log(user, msg);
+    io.to(user.room).emit('message', formatMessage(user.username, msg));
+  });
+
+  // Runs when client disconnects
+  socket.on('disconnect', () => {
+    const user = userLeave(socket.id);
+
+    if (user) {
+      io.to(user.room).emit(
+        'message',
+        formatMessage(botName, `${user.username} has left the chat`)
+      );
+
+      // Send users and room info
+      io.to(user.room).emit('roomUsers', {
+        room: user.room,
+        users: getRoomUsers(user.room)
+      });
+    }
+  });
+});
+
+function formatMessage(username, text) {
+  return {
+    username,
+    text,
+    time:'hehe'
+  };
+}
+
+const users = [];
+
+// Join user to chat
+function userJoin(id, username, room) {
+  const user = { id, username, room };
+
+  users.push(user);
+
+  return user;
+}
+
+// Get current user
+function getCurrentUser(id) {
+  return users.find(user => user.id === id);
+}
+
+// User leaves chat
+function userLeave(id) {
+  const index = users.findIndex(user => user.id === id);
+
+  if (index !== -1) {
+    return users.splice(index, 1)[0];
+  }
+}
+
+// Get room users
+function getRoomUsers(room) {
+  return users.filter(user => user.room === room);
+}
+
