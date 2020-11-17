a0ac07704bd357065090222d3f8ae0c9bccb57ff
LiisKasper <liis.kasper@gmail.com>
2020-11-17 10:25:17 +0000

    server2

--- server2.js
+++ server2.js
@@ -42,30 +42,43 @@ io.on('connection', (socket) => {
             console.log({ E: 'Talking before entering...', user_id, room_name, message })
             return
         }
+        ROOMPOOL[room_name].messages.push({user_id: user_id, message: message})
+        console.log((JSON.stringify({ROOMPOOL}, null, 4)));
         io.to(room_name)
-            .emit('messageToClient', formatMessage(USERPOOL[user_id].user_name, msg))
+            .emit('messageToClient', formatMessage(user_id, message))
     })
 
     socket.on('disconnect', () => {
         const user_id = SOCKET_USER[socket.id]
-
+        console.log('Disconnecting', {user_id, SOCKET_USER});
         if (user_id) {
             const user_name = USERPOOL[user_id].user_name
             const user_rooms = USER_ROOMS[user_id] || []
             for (const room_name of user_rooms) {
                 io.to(room_name)
-                    .emit('messageToClient', formatMessage(null, `- ${user_name}`))
-    
+                .emit('messageToClient', formatMessage(null, `- ${user_name}`))
+                console.log('Before', ROOMPOOL)
+                removeUserFromRoompool(room_name, user_id)
+                console.log('After', ROOMPOOL)
                 broadcastRoomUsers(room_name)
             }
+            USER_ROOMS[user_id] = []
+            console.log('Disconnecting', {socket: socket.id, user: SOCKET_USER[socket.id], SOCKET_USER});
+            delete(SOCKET_USER[socket.id])
         }
     })
 })
 
+function removeUserFromRoompool(room_name, user_id) {
+    let room_users = ROOMPOOL[room_name].users
+    let user_index = room_users.indexOf(user_id)
+    room_users.splice(user_index, 1)
+}
+
 function joinUserToRoom(incoming_object, socket) {
-    const user_id = incoming_object.userProfile.email
-    const user_name = incoming_object.userName
-    const room_name = incoming_object.roomName
+    const user_id = incoming_object.user_id
+    const user_name = incoming_object.user_name
+    const room_name = incoming_object.room_name
     const userProfile = incoming_object.userProfile
     console.log('joinRoom', {user_id, user_name, room_name})
     socket.join(room_name) // Nüüd on see socket seotud konkreetse nimeruumiga https://socket.io/docs/v3/rooms/index.html 
@@ -83,7 +96,7 @@ function joinUserToRoom(incoming_object, socket) {
     
     socket.emit('messageToClient', formatMessage(user_id, 'tere-tere'))
     
-    const previous_messages = ROOMPOOL[room_name].messages.slice(-10, -1)
+    const previous_messages = ROOMPOOL[room_name].messages.slice(-10)
     for (message of previous_messages) {
         socket.emit('messageToClient', formatMessage(message.user_id, message.message))
     }
@@ -112,7 +125,7 @@ function formatMessage(user_id, text) {
         try {
             user_name = USERPOOL[user_id].user_name
         } catch (error) {
-            console.log({ E: error, user_id })
+            console.log({ E: error, user_id, USERPOOL })
         }
     }
     return {
