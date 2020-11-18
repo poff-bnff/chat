b721f261929100152bc1fe117f7655dc5cfd1a8b
LiisKasper <liis.kasper@gmail.com>
2020-11-18 10:50:15 +0000

    1.0.0

--- server.js
+++ server.js
@@ -6,42 +6,46 @@ const moment = require('moment')
 
 const server = require('http').createServer()
 const options = {
-    pingInterval: 10000,
-    pingTimeout: 5000
+    pingInterval: 0.5*60*1000,
+    pingTimeout: 5*1000
 }
 const io = require('socket.io')(server, options)
 server.listen(3000)
 
-const SOCKETPOOL = {}
+var SOCKETPOOL = {}
 const socketpool_filepath = path.join(__dirname, 'socketpool.yaml')
-if (!fs.existsSync(socketpool_filepath)) {
-    fs.writeFileSync(socketpool_filepath, '{}', 'utf8')
-}
+fs.writeFileSync(socketpool_filepath, '{}', 'utf8')
 function saveSocketPool() {
     fs.writeFileSync(socketpool_filepath, yaml.safeDump(JSON.parse(JSON.stringify(SOCKETPOOL)), { 'noRefs': true, 'indent': '4' }), 'utf8')
 }
 
-const USERPOOL = {}
+var USERPOOL = {}
 const userpool_filepath = path.join(__dirname, 'userpool.yaml')
-if (!fs.existsSync(userpool_filepath)) {
+if (fs.existsSync(userpool_filepath)) {
+    USERPOOL = yaml.safeLoad(fs.readFileSync(userpool_filepath, 'utf8'))
+} else {
     fs.writeFileSync(userpool_filepath, '{}', 'utf8')
 }
 function saveUserPool() {
     fs.writeFileSync(userpool_filepath, yaml.safeDump(USERPOOL, { 'noRefs': true, 'indent': '4' }), 'utf8')
 }
 
-const ROOMPOOL = {}
+var ROOMPOOL = {}
 const roompool_filepath = path.join(__dirname, 'roompool.yaml')
-if (!fs.existsSync(roompool_filepath)) {
+if (fs.existsSync(roompool_filepath)) {
+    ROOMPOOL = yaml.safeLoad(fs.readFileSync(roompool_filepath, 'utf8'))
+} else {
     fs.writeFileSync(roompool_filepath, '{}', 'utf8')
 }
 function saveRoomPool() {
     fs.writeFileSync(roompool_filepath, yaml.safeDump(ROOMPOOL, { 'noRefs': true, 'indent': '4' }), 'utf8')
 }
 
-const MESSAGEPOOL = {}
+var MESSAGEPOOL = {}
 const messagepool_filepath = path.join(__dirname, 'messagetpool.yaml')
-if (!fs.existsSync(messagepool_filepath)) {
+if (fs.existsSync(messagepool_filepath)) {
+    MESSAGEPOOL = yaml.safeLoad(fs.readFileSync(messagepool_filepath, 'utf8'))
+} else {
     fs.writeFileSync(messagepool_filepath, '{}', 'utf8')
 }
 function saveMessagePool() {
@@ -49,14 +53,14 @@ function saveMessagePool() {
 }
 
 
-
-const SOCKET_USER = {}
-const USER_ROOMS = {}
-
 io.on('connection', (socket) => {
     console.log('a user connected', socket.id)
-    SOCKETPOOL[socket.id] = {}
+    SOCKETPOOL[socket.id] = null
     saveSocketPool()
+
+    // socket.conn.on('packetCreate', function (packet) {
+    //     if (packet.type === 'pong') console.log('=== sending pong');
+    // })
 
     socket.on('joinRoom', (incoming_object) => {
         let access_level = 'user'
@@ -73,9 +77,7 @@ io.on('connection', (socket) => {
         SOCKETPOOL[socket.id] = {user_id, room_name, is_moderated}
         saveSocketPool()
     
-        
-        SOCKET_USER[socket.id] = user_id
-        USERPOOL[user_id] = { socket_id: socket.id, user_name, access_level } //, userProfile }
+        USERPOOL[user_id] = { user_name, access_level } //, userProfile }
         saveUserPool()
     
         ROOMPOOL[room_name] = ROOMPOOL[room_name] || { users: [], messages: [] }
@@ -83,22 +85,20 @@ io.on('connection', (socket) => {
         ROOMPOOL[room_name].users = [...new Set(ROOMPOOL[room_name].users)]
         saveRoomPool()
     
-        USER_ROOMS[user_id] = USER_ROOMS[user_id] || []
-        USER_ROOMS[user_id].push(room_name)
-        USER_ROOMS[user_id] = [...new Set(USER_ROOMS[user_id])]
-    
-        socket.emit('messageToClient', formatMessage(user_id, 'tere-tere'))
+        // socket.emit('messageToClient', formatMessage(user_id, 'tere-tere'))
     
         const previous_messages = ROOMPOOL[room_name].messages.slice(-10)
-        for (message of previous_messages) {
-            socket.emit('messageToClient', message.message)
+        for (message_id of previous_messages) {
+            let message = MESSAGEPOOL[message_id]
+            console.log('sending previous', {message})
+            socket.emit('messageToClient', message)
         }
         // console.log(JSON.stringify({ USERPOOL, ROOMPOOL, USER_ROOMS, previous_messages }, null, 4))
     
         // k6igile v4lja arvatud kasutaja ise 
-        socket.broadcast
-            .to(room_name)
-            .emit('messageToClient', formatMessage(null, `+ ${user_name}`))
+        // socket.broadcast
+        //     .to(room_name)
+        //     .emit('messageToClient', formatMessage(null, `+ ${user_name}`))
     
         // Send users and room info
         broadcastRoomUsers(room_name)
@@ -109,58 +109,71 @@ io.on('connection', (socket) => {
         console.log('got messageToServer', { socket: socket.id, user_id, room_name, message });
         if (!USERPOOL[user_id] || !ROOMPOOL[room_name] || !ROOMPOOL[room_name].users) {
             console.log({ E: 'Talking before entering...', user_id, room_name, message })
+            socket.emit('Rejoin, please')
             return
         }
         const formatted_message = formatMessage(user_id, message)
         MESSAGEPOOL[formatted_message.id] = {user_id, room_name, ...formatted_message}
         saveMessagePool()
 
-        ROOMPOOL[room_name].messages.push({ user_id: user_id, message: formatted_message })
+        ROOMPOOL[room_name].messages.push(formatted_message.id)
+        ROOMPOOL[room_name].messages = [...new Set(ROOMPOOL[room_name].messages)]
         saveRoomPool()
         // console.log((JSON.stringify({ROOMPOOL}, null, 4)));
         io.to(room_name)
-            .emit('messageToClient', formatted_message)
+            .emit('messageToClient', MESSAGEPOOL[formatted_message.id])
     })
 
     socket.on('moderate', (message_id) => {
-        ROOMPOOL[room_name].messages
-            .filter(m => m.id === message_id)
-            .forEach(message_to_mod => {
-                if (!message_to_mod.is_moderated) {
-                    message_to_mod.is_moderated = true
-                    
-                }
-            })
-        saveRoomPool()
+        let message = MESSAGEPOOL[message_id]
+        message.is_moderated = true
+        console.log('kas tõesti', {message, MESSAGEPOOL})
+        saveMessagePool()
+        io.to(message.room_name)
+            .emit('messageToClient', message)
     })
 
     socket.on('disconnect', () => {
-        const user_id = SOCKET_USER[socket.id]
-        try {
-            const user_name = USERPOOL[user_id].user_name
-            const user_rooms = USER_ROOMS[user_id] || []
-            for (const room_name of user_rooms) {
-                io.to(room_name)
-                    .emit('messageToClient', formatMessage(null, `- ${user_name}`))
-                removeUserFromRoompool(room_name, user_id)
-                broadcastRoomUsers(room_name)
-            }
-            USER_ROOMS[user_id] = []
-            console.log('Disconnecting', { user_id, socket: socket.id });
-            delete (SOCKET_USER[socket.id])
-            delete (SOCKETPOOL[socket.id])
-            saveSocketPool()
-        } catch (e) {
-            console.log('not a user (anymore?)', {socket_id: socket.id, user_id})
+        let socket_id = socket.id
+        if (SOCKETPOOL[socket_id] === null) {
+            disconnectSocket(socket)
+            console.log('close empty connection', socket_id)
+            return
         }
+        let room_name = SOCKETPOOL[socket_id].room_name
+        let user_id = SOCKETPOOL[socket_id].user_id
+        let user_name = USERPOOL[user_id].user_name
+        console.log('Disconnecting', { socket_id, user_id, room_name })
+        console.log('Disconnecting', JSON.stringify({ user: USERPOOL[user_id], room: ROOMPOOL[room_name] }, null, 4))
+        removeUserFromRoompool(room_name, user_id)
+        disconnectSocket(socket)
+
+        io.to(room_name)
+            .emit('messageToClient', formatMessage(null, `- ${user_name}`))
+        removeUserFromRoompool(room_name, user_id)
+        broadcastRoomUsers(room_name)
     })
 })
 
 
+function disconnectSocket(socket) {
+    try {
+        delete (SOCKETPOOL[socket.id])
+        saveSocketPool()
+    } catch (e) {
+        console.log('cannot remove socket', socket.id, 'from pool', SOCKETPOOL)
+    }
+}
+
 function removeUserFromRoompool(room_name, user_id) {
-    let room_users = ROOMPOOL[room_name].users
-    let user_index = room_users.indexOf(user_id)
-    room_users.splice(user_index, 1)
+    try {
+        let room_users = ROOMPOOL[room_name].users
+        let user_index = room_users.indexOf(user_id)
+        room_users.splice(user_index, 1)
+        saveRoomPool()
+    } catch (e) {
+        console.log('cannot remove user', user_id, 'from room', ROOMPOOL)
+    }
 }
 
 function broadcastRoomUsers(room_name) {
