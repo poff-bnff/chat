81cde16a0caaf920dd59707e393525eff156dbe7
LiisKasper <liis.kasper@gmail.com>
2020-11-19 14:10:54 +0000

    user data from cognito

--- test_server.js
+++ test_server.js
@@ -2,13 +2,11 @@ const path = require('path')
 const fs = require('fs')
 const yaml = require('js-yaml')
 const moment = require('moment')
-const https = require('https')
-const COGNITO_PROFILE_URL = 'https://api.poff.ee/trigger/cognitoprof?sub='
 
 const server = require('http').createServer()
 const options = {
-    pingInterval: 0.5 * 60 * 1000,
-    pingTimeout: 5 * 1000
+    pingInterval: 0.5*60*1000,
+    pingTimeout: 5*1000
 }
 const io = require('socket.io')(server, options)
 server.listen(3000)
@@ -37,65 +35,27 @@ io.on('connection', (socket) => {
     // socket.conn.on('packetCreate', function (packet) {
     //     if (packet.type === 'pong') console.log('=== sending pong');
     // })
-    async function lookupUser(user_id) {
-        return new Promise((resolve, reject) => {
-            https.get(COGNITO_PROFILE_URL + user_id, (res) => {
-                const { statusCode } = res
-                const contentType = res.headers['content-type']
-                console.log({ statusCode, contentType })
-                let error
-                if (statusCode !== 200) {
-                    error = new Error(`Request Failed.\nStatus Code: ${statusCode}`)
-                } else if (contentType !== 'application/json') {
-                    error = new Error(`Invalid content-type.\nExpected "application/json" but received "${contentType}"`)
-                }
-                if (error) {
-                    console.error({ 'Error message': error.message, error })
-                    reject(error)
-                }
 
-                res.setEncoding('utf8')
-                let rawData = ''
-                res.on('data', (chunk) => {
-                    rawData += chunk
-                })
-                res.on('end', () => {
-                    // console.log(rawData)
-                    json_data = JSON.parse(rawData)
-                    resolve({user_name: json_data.name + ' ' + json_data.family_name, access_level: getAccessLevel(user_id)})
-                })
-            }).on('error', (e) => {
-                console.error(`Got error: ${e.message}`)
-            })
-        })
-
-        // const fetched = {"name":"Jaan","family_name":"Leppik","email":"jaan.leppik@poff.ee","industryUser":true}
-        // if (!fetched.industryUser) {
-        //     return false
-        // }
-        // return {user_name: fetched.name + ' ' + fetched.family_name, access_level: getAccessLevel(user_id)}
-    }
-
-    socket.on('joinRoom', async (incoming_object) => {
-        const room_name = incoming_object.room_name
+    socket.on('joinRoom', (incoming_object) => {
         const user_id = incoming_object.user_id
+        const access_level = getAccessLevel(user_id)
+        const user_name = incoming_object.user_name
+        const room_name = incoming_object.room_name
+        const is_moderated = incoming_object.is_moderated
         console.log('join user', user_id, 'to room', room_name)
-        const socket_user = await lookupUser(user_id) // {"user_name":"Jaan Leppik","access_level":"moderator"}
-        console.log({ socket_user })
-
         socket.join(room_name) // Nüüd on see socket seotud konkreetse nimeruumiga https://socket.io/docs/v3/rooms/index.html 
 
-        SOCKETPOOL[socket.id] = { user_id, room_name } //, is_moderated}
+        SOCKETPOOL[socket.id] = {user_id, room_name, is_moderated}
         saveSocketPool()
-
-        USERPOOL[user_id] = { user_name: socket_user.user_name, access_level: socket_user.access_level } //, userProfile }
+    
+        USERPOOL[user_id] = { user_name, access_level } //, userProfile }
         saveUserPool()
-
+    
         ROOMPOOL[room_name] = ROOMPOOL[room_name] || { users: [], messages: [] }
         ROOMPOOL[room_name].users.push(user_id)
         ROOMPOOL[room_name].users = [...new Set(ROOMPOOL[room_name].users)]
         saveRoomPool()
-
+    
         const previous_messages = ROOMPOOL[room_name].messages.slice(-10)
         for (message_id of previous_messages) {
             let message = MESSAGEPOOL[message_id]
@@ -105,7 +65,7 @@ io.on('connection', (socket) => {
         // socket.broadcast
         //     .to(room_name)
         //     .emit('broadcast', formatMessage(null, `+ ${user_name}`))
-
+    
         // Send users and room info
         broadcastRoomUsers(room_name)
     })
@@ -119,7 +79,7 @@ io.on('connection', (socket) => {
             return
         }
         const formatted_message = formatMessage(user_id, message)
-        MESSAGEPOOL[formatted_message.id] = { user_id, room_name, ...formatted_message }
+        MESSAGEPOOL[formatted_message.id] = {user_id, room_name, ...formatted_message}
         saveMessagePool()
 
         ROOMPOOL[room_name].messages.push(formatted_message.id)
@@ -133,7 +93,7 @@ io.on('connection', (socket) => {
     socket.on('moderate', (message_id) => {
         let message = MESSAGEPOOL[message_id]
         message.is_moderated = true
-        console.log('kas tõesti', { message, MP: MESSAGEPOOL[message_id] })
+        console.log('kas tõesti', {message, MESSAGEPOOL})
         saveMessagePool()
         io.to(message.room_name)
             .emit('messageToClient', message)
@@ -149,7 +109,7 @@ io.on('connection', (socket) => {
         let room_name = SOCKETPOOL[socket_id].room_name
         let user_id = SOCKETPOOL[socket_id].user_id
         let user_name = USERPOOL[user_id].user_name
-        console.log('Disconnecting', socket_id, user_id, 'from', room_name)
+        console.log('Disconnecting', socket_id, user_id, 'from', room_name )
         removeUserFromRoompool(room_name, user_id)
         disconnectSocket(socket)
 
@@ -209,8 +169,7 @@ function formatMessage(user_id, text) {
 
 // ---------------------
 function getAccessLevel(user_id) {
-    console.log(user_id, 'at', MODERATORS, 'is', MODERATORS.indexOf(user_id))
-    return MODERATORS.indexOf(user_id) < 0 ? null: 'moderator'
+    return MODERATORS.indexOf(user_id) || null
 }
 function initializeModerators() {
     const moderators_filepath = path.join(__dirname, 'moderators.yaml')
